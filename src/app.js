7baaddf66717fcebf7d59a2095f2666c4b6a053e
jihyeon-Do <ehwlgus22@gmail.com>
2025-03-25 02:44:34 +0900

    fix : cors error

--- src/app.js
+++ src/app.js
@@ -3,10 +3,20 @@ import dotenv from 'dotenv';
 import cafeRouter from './routes/cafeRoute.js';
 import commentRouter from './routes/commentRoute.js';
 import categoryRouter from './routes/categoryRoute.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // 허용할 도메인
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+  }),
+);
 
 //: 라우터 연결
 app.use('/api', cafeRouter);
